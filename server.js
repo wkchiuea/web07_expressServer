35b82bdda73a9bb4dd0aa56493ad3ab0dd4cc31a
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-23 09:41:36 +0800

    [update] add app.get

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 
 const app = express();
 
+// specify what would happen when browser get in touch with our server
+// / : location = root
+app.get("/", function(request, response) {
+    response.send("<h1>Hello</h1>");
+});
+
 // app will listen to port 3000 for http request send to our server
 app.listen(3000, function() {
     
